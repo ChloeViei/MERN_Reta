8f97e1ce9518577834a80674c91568b29c225d41
Chloé VIEILLE <chloevieille@hotmail.com>
2021-10-24 19:54:24 +0200

    CRUD User

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,7 +5,12 @@ const userController = require('../controllers/user.controller');
 // auth
 router.post('/register', authController.signUp);
 
-// user display: 'block'
+// user
 router.get('/', userController.getAllUsers);
+router.get('/:id', userController.userInfo);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+router.patch('/follow/:id', userController.follow);
+router.patch('/unfollow/:id', userController.unfollow);
 
 module.exports = router;
